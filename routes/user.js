e65fd70be71f976d856c7df549fbe684a44ae70d
unknown <501981226@qq.com>
2018-06-27 16:47:42 +0800

    2018062702

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const UserService=require('../Service/UserService');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+//用户登录
+router.post('/login',function (req,res,next) {
+  UserService.SignIn(req.body,function(data){
+    return res.json(data);
+  });
+});
+
+//用户注册
+router.post('/register',function (req,res,next) {
+  UserService.SignUp(req.body,function (data) {
+    return res.json(data);
+  });
+});
+
+//获取除admin之后的，所有用户列表
+router.get('/list',function (req,res,next) {
+  UserService.list(req.query,function(data){
+    return res.json(data);
+  });
 });
 
 module.exports = router;
